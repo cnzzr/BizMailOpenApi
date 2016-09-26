35edde2d5672627ed812bba08cbc1a44c4aa059b
02166 <zzr@msdi.cn>
2016-09-26 15:31:13 +0800

    feat(model): 邮箱成员帐号实体
    完善实体属性以适用于资料更新方法

--- src/main/java/com/qq/exmail/openapi/model/BizUser.java
+++ src/main/java/com/qq/exmail/openapi/model/BizUser.java
@@ -1,24 +1,28 @@
 package com.qq.exmail.openapi.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jodd.json.meta.JSON;
 
 import com.qq.exmail.openapi.BaseModel;
 import com.qq.exmail.openapi.model.jodd.Result;
 
 /**
- * 
+ * 成员资料对象
  * Alias 帐号名
  * Name 姓名
  * Gender 性别：0=未设置，1=男，2=女
- * SlaveList 别名列表，用逗号分隔
  * Position 职位
  * Tel 联系电话
  * Mobile 手机
  * ExtId 编号
- * PartyList 部门列表，部门的根结点不包括在路径里面。比如部门所属： 腾
- * 讯/广州研发中心/企业邮箱，Value 为：广州研发中心/企业邮箱
  * OpenType 成员状态：1=启用，2=禁用
  * 
+ * <p>查询时</br>
+ * PartyList 部门列表，部门的根结点不包括在路径里面。比如部门所属： 腾讯/广州研发中心/企业邮箱，Value 为：广州研发中心/企业邮箱</br>
+ * SlaveList 别名列表，用逗号分隔
+ * </p> 
  * @author 张宗荣
  * 
  */
@@ -29,8 +33,6 @@ public final class BizUser extends BaseModel{
 	private String name;
 	@JSON(name = "Gender")
 	private String gender;
-	@JSON(name = "SlaveList")
-	private String slaveList;
 	@JSON(name = "Position")
 	private String position;
 	@JSON(name = "Tel")
@@ -39,19 +41,27 @@ public final class BizUser extends BaseModel{
 	private String mobile;
 	@JSON(name = "ExtId")
 	private String extId;
-	
-	@JSON(name = "PartyPath")
-	private String partyPath;
-	
 	@JSON(name = "OpenType")
 	private String openType;
+	
+	@JSON(name = "PartyPath")
+	private List<String> partyPath;	//替换为对象，所属部门
+	@JSON(name="Slave")
+	private List<String> slave;
+	
 	
 	//用于员工密码的属性
 	@JSON(name = "Password")
 	private String password;
-	
+
+	//---------------- 以下属于用于 获取成员资料后执行序列化
 	@JSON(name = "PartyList")
 	private Result PartyList;
+	@JSON(name = "SlaveList")
+	/**
+	 * 如："SlaveList": "bb@gzdev.com,bo@gzdev.com"
+	 */
+	private String slaveList;
 	
 	public String getPassword() {
 		return password;
@@ -78,12 +88,7 @@ public final class BizUser extends BaseModel{
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	public String getSlaveList() {
-		return slaveList;
-	}
-	public void setSlaveList(String slaveList) {
-		this.slaveList = slaveList;
-	}
+
 	public String getPosition() {
 		return position;
 	}
@@ -108,20 +113,61 @@ public final class BizUser extends BaseModel{
 	public void setExtId(String extId) {
 		this.extId = extId;
 	}
-	public String getPartyPath() {
+	
+	public List<String> getPartyPath() {
 		return partyPath;
 	}
-	
-	//TODO 替换为对象，所属部门
-	/*
+	/**
+	 * 批量设置所属部门
+	 * 1、传部门路径，用’/’分隔
+	 * 2、根部门不需要传。如果空，则为根部门。部门要求是已存在的
+	 * @param partyPath
+	 */
+	public void setPartyPath(List<String> partyPath) {
+		this.partyPath = partyPath;
+	}
+	/**
 	 * 设置所属部门
 	 * 1、传部门路径，用’/’分隔
 	 * 2、根部门不需要传。如果空，则为根部门。部门要求是已存在的
-	 * 3、如果是多个部门，传多个PartyPath
+	 * 3、如果属于多个部门，则添加多个PartyPath
+	 * @param partyPath
 	 */
-	public void setPartyPath(String partyPath) {
-		this.partyPath = partyPath;
+	public void addPartypath(String partyPath){
+		if(null == this.partyPath){
+			this.partyPath = new ArrayList<String>();
+		}
+		this.partyPath.add(partyPath);
 	}
+	
+	public List<String> getSlave() {
+		return slave;
+	}
+	/**
+	 * 设置别名，注意系统限制五个
+	 */
+	@Deprecated
+	public void setSlave(List<String> slave) {
+		this.slave = slave;
+	}
+
+	/**
+	 * 添加单个别名，系统上限5个
+	 * 
+	 * @param slave
+	 * @return true成功、false失败
+	 */
+	public boolean addSlave(String slave) {
+		if (null == this.slave) {
+			this.slave = new ArrayList<String>();
+		}
+		if (this.slave.size() >= 5 && !this.slave.contains(slave)) {
+			return false;
+		}
+		this.slave.add(slave);
+		return true;
+	}
+	
 	public String getOpenType() {
 		return openType;
 	}
@@ -129,18 +175,33 @@ public final class BizUser extends BaseModel{
 		this.openType = openType;
 	}
 	
+	/**
+	 * 别名列表，用逗号分隔
+	 * @return
+	 */
+	public String getSlaveList() {
+		return slaveList;
+	}
+	/**
+	 * 仅供序列化调用
+	 * @param slaveList
+	 */
+	public void setSlaveList(String slaveList) {
+		this.slaveList = slaveList;
+	}
+	/**
+	 * 复杂对象：部门列表，部门的根结点不包括在路径里面。
+	 * @return
+	 */
 	public Result getPartyList() {
 		return PartyList;
 	}
+	/**
+	 * 仅供序列化调用
+	 * @param partyList
+	 */
 	public void setPartyList(Result partyList) {
 		PartyList = partyList;
 	}
 	
-	
-//	@JSON(include=false)
-//	Field[] _fields = ReflectUtil.getSupportedFields(getClass());
-//	@Override
-//	protected Field[] getField() {
-//		return _fields;
-//	}
 }
